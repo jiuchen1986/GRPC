b2187c6f5057b3242f2b86a5704842d0267dea75
Xin Chen <xin.e.chen@ericsson.com>
2018-05-30 20:27:04 +0800

    complete the implementations of the client

--- src/mygrpc/mygrpcimpl/client/clientimpl.go
+++ src/mygrpc/mygrpcimpl/client/clientimpl.go
@@ -7,6 +7,7 @@ import (
     "fmt"
     "log"
     "os"
+    "io"
     "time"
     
     pb "mygrpc/mygrpc"
@@ -52,7 +53,7 @@ type routineChannel struct {
 
 func (r *routineChannel) Wait(ci, ct time.Duration, cn int) error {
     go func() {
-        time.Sleep(time.Duration((int(ci) + int(ct)) * (cn - 1) + int(ct)))
+        time.Sleep(time.Duration((int(ci) + int(ct)) * cn + int(ct)))
         r.timeoutChannel <- true
     }()
     
@@ -102,6 +103,12 @@ func (c *myGrpcClientSet) Run() error {
             switch c.rpcType {
                 case "simple":
                     go c.CallSimpleRPC(client, cr.endChannel, cr.routineId, cr.clientId)
+                case "server_stream":
+                    go c.CallServerStreamRPC(client, cr.endChannel, cr.routineId, cr.clientId)
+                case "client_stream":
+                    go c.CallClientStreamRPC(client, cr.endChannel, cr.routineId, cr.clientId)
+                case "bi_stream":
+                    go c.CallBiStreamRPC(client, cr.endChannel, cr.routineId, cr.clientId)
             }
             cr.Wait(c.callInterval, c.callTimeout, c.callNum)
         }
@@ -116,21 +123,173 @@ func (c *myGrpcClientSet) CallSimpleRPC(client pb.MyGrpcClient, ech chan struct{
         cfs := make([]context.CancelFunc, c.callNum)  // cancel function list for the context in each calling
         ctxs := make([]context.Context, c.callNum)  // context list for each calling
         for k := 0; k < c.callNum; k++ {
-            ctxs[k], cfs[k] = context.WithTimeout(context.Background(), c.callTimeout)            
-        }
+            ctxs[k], cfs[k] = context.WithTimeout(context.Background(), c.callTimeout + time.Duration(k * int(c.callInterval)))
+        }            
         defer release(ech, cfs)
         
         for k := 0; k < c.callNum; k++ {
             scd, err := client.GetChainReqResp(ctxs[k], c.testChainInfo[k%len(c.testChainInfo)])
             if err != nil {
-                myGrpcLogger.Fatalf("Failed to call simple rpc by goroutine %d in client %d: %v", rid, cid, err)
+                myGrpcLogger.Fatalf("Failed to call simple rpc by goroutine %d in client %d for call number %d: %v", rid, cid, k, err)
             }
             jsonStr, _ := json.Marshal(scd)
-            myGrpcLogger.Printf("Get a response by goroutine %d in client %d: %s", rid, cid, string(jsonStr))
+            myGrpcLogger.Printf("Get a response from simple rpc by goroutine %d in client %d for call number %d: %s", rid, cid, k, string(jsonStr))
+            
+            time.Sleep(c.callInterval)
         }
         
-        return nil
+        return nil        
+    }
+    
+    myGrpcLogger.Printf("Current implementations only support using test data, requests are pass")
+    close(ech)
+    return nil
+}
+
+func (c *myGrpcClientSet) CallServerStreamRPC(client pb.MyGrpcClient, ech chan struct{}, rid, cid int) error {    
+    if c.useTestFile {
+        myGrpcLogger.Printf("Calling server-streaming rpc by goroutine %d in client %d", rid, cid)
+        scl := make([]*pb.ServiceChain, len(c.testChainInfo))
+        for l, sc := range c.testChainInfo {
+            scl[l] = sc
+        }
+        scs := &pb.ServiceChains {
+                   Chains: scl,
+               }
         
+        cfs := make([]context.CancelFunc, c.callNum)  // cancel function list for the context in each calling
+        ctxs := make([]context.Context, c.callNum)  // context list for each calling
+        for k := 0; k < c.callNum; k++ {
+            ctxs[k], cfs[k] = context.WithTimeout(context.Background(), c.callTimeout + time.Duration(k * int(c.callInterval)))
+        }            
+        defer release(ech, cfs)
+        
+        var stream pb.MyGrpc_GetChainsReqRespsClient
+        var err error
+        for k := 0; k < c.callNum; k++ {
+            
+            stream, err = client.GetChainsReqResps(ctxs[k], scs)
+            if err != nil {
+                myGrpcLogger.Fatalf("Failed to call server-streaming rpc by goroutine %d in client %d for call number %d: %v", rid, cid, k, err)
+            }
+            for {
+                scd, er := stream.Recv()
+                if er == io.EOF {
+                    break
+                }
+                if er != nil {
+                    myGrpcLogger.Fatalf("Failed to receive from server-streaming rpc by goroutine %d in client %d for call number %d: %v", rid, cid, k, er)
+                }
+                jsonStr, _ := json.Marshal(scd)
+                myGrpcLogger.Printf("Get a response from server-streaming rpc by goroutine %d in client %d for call number %d: %s", rid, cid, k, string(jsonStr))
+            }
+
+            time.Sleep(c.callInterval)
+        }
+        
+        return nil        
+    }
+    
+    myGrpcLogger.Printf("Current implementations only support using test data, requests are pass")
+    close(ech)
+    return nil
+}
+
+func (c *myGrpcClientSet) CallClientStreamRPC(client pb.MyGrpcClient, ech chan struct{}, rid, cid int) error {    
+    if c.useTestFile {
+        myGrpcLogger.Printf("Calling client-streaming rpc by goroutine %d in client %d", rid, cid)
+        cfs := make([]context.CancelFunc, c.callNum)  // cancel function list for the context in each calling
+        ctxs := make([]context.Context, c.callNum)  // context list for each calling
+        for k := 0; k < c.callNum; k++ {
+            ctxs[k], cfs[k] = context.WithTimeout(context.Background(), c.callTimeout + time.Duration(k * int(c.callInterval)))
+        }            
+        defer release(ech, cfs)
+        
+        var stream pb.MyGrpc_GetChainsReqsRespClient
+        var err error        
+        for k := 0; k < c.callNum; k++ {
+            
+            stream, err = client.GetChainsReqsResp(ctxs[k])
+            if err != nil {
+                myGrpcLogger.Fatalf("Failed to call client-streaming rpc by goroutine %d in client %d for call number %d: %v", rid, cid, k, err)
+            }
+            for _, sc := range c.testChainInfo {
+                er := stream.Send(sc)
+                if er != nil {
+                    myGrpcLogger.Fatalf("Failed to send to client-streaming rpc by goroutine %d in client %d for call number %d: %v", rid, cid, k, er)
+                }
+                jsonStr, _ := json.Marshal(sc)
+                myGrpcLogger.Printf("Send to client-streaming rpc by goroutine %d in client %d for call number %d: %s", rid, cid, k, string(jsonStr))
+            }
+            
+            scds, er := stream.CloseAndRecv()
+            if er != nil {
+                myGrpcLogger.Fatalf("Failed to receive from client-streaming rpc by goroutine %d in client %d for call number %d: %v", rid, cid, k, er)
+            }
+            jsonStr, _ := json.Marshal(scds)
+            myGrpcLogger.Printf("Get a response from client-streaming rpc by goroutine %d in client %d for call number %d: %s", rid, cid, k, string(jsonStr))
+            
+            time.Sleep(c.callInterval)
+        }
+        
+        return nil        
+    }
+    
+    myGrpcLogger.Printf("Current implementations only support using test data, requests are pass")
+    close(ech)
+    return nil
+}
+
+func (c *myGrpcClientSet) CallBiStreamRPC(client pb.MyGrpcClient, ech chan struct{}, rid, cid int) error {    
+    if c.useTestFile {
+        myGrpcLogger.Printf("Calling bi-streaming rpc by goroutine %d in client %d", rid, cid)
+        cfs := make([]context.CancelFunc, c.callNum)  // cancel function list for the context in each calling
+        ctxs := make([]context.Context, c.callNum)  // context list for each calling
+        for k := 0; k < c.callNum; k++ {
+            ctxs[k], cfs[k] = context.WithTimeout(context.Background(), c.callTimeout + time.Duration(k * int(c.callInterval)))
+        }            
+        defer release(ech, cfs)
+        
+        var stream pb.MyGrpc_GetChainsReqsRespsClient
+        var err error        
+        for k := 0; k < c.callNum; k++ {
+            
+            stream, err = client.GetChainsReqsResps(ctxs[k])
+            if err != nil {
+                myGrpcLogger.Fatalf("Failed to call bi-streaming rpc by goroutine %d in client %d for call number %d: %v", rid, cid, k, err)
+            }
+            waitch := make(chan struct {})
+            go func() {
+                for {
+                    scd, er := stream.Recv()
+                    if er == io.EOF {
+                        close(waitch)
+                        return
+                    }
+                    if er != nil {
+                        myGrpcLogger.Fatalf("Failed to receive from server-streaming rpc by co-goroutine of goroutine %d in client %d for call number %d: %v", rid, cid, k, er)
+                    }
+                    jsonStr, _ := json.Marshal(scd)
+                    myGrpcLogger.Printf("Get a response from server-streaming rpc by co-goroutine of goroutine %d in client %d for call number %d: %s", rid, cid, k, string(jsonStr))
+                }
+            }()
+            
+            for _, sc := range c.testChainInfo {
+                er := stream.Send(sc)
+                if er != nil {
+                    myGrpcLogger.Fatalf("Failed to send to bi-streaming rpc by goroutine %d in client %d for call number %d: %v", rid, cid, k, er)
+                }
+                jsonStr, _ := json.Marshal(sc)
+                myGrpcLogger.Printf("Send to client-streaming rpc by goroutine %d in client %d for call number %d: %s", rid, cid, k, string(jsonStr))
+                time.Sleep(1e6)
+            }
+            stream.CloseSend()  // function of the grpc.ClientStream interface, which is the part of the interface combination composing pb.MyGrpc_GetChainsReqsRespsClient interface
+            <-waitch
+            
+            time.Sleep(c.callInterval)
+        }
+        
+        return nil        
     }
     
     myGrpcLogger.Printf("Current implementations only support using test data, requests are pass")
